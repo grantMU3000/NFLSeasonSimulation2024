225cfcda13ea3bb0b6ba6274dee286f94a83f59f
Grant <robinsonjg64@gmail.com>
2025-02-22 07:36:06 -0500

    Added in all of my NFC teams

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -1,12 +1,28 @@
-INSERT INTO Teams (City, Mascot, Division, Conference)
+Create Table NFC (
+	teamID int Primary Key,
+    City varchar(50),
+    Mascot varchar(50) Unique,
+    Wins int,
+    Losses int,
+    ConferenceWins int,
+    ConferenceLosses int,
+    Division varchar(25)
+);
 
-Values ('Pittsburgh', 'Steelers', 'AFC North', 'AFC'),
-		('San Francisco', '49ers', 'NFC West', 'NFC'),
-        ('Seattle', 'Seahawks', 'NFC West', 'NFC'),
-        ('Tampa Bay', 'Buccaneers', 'NFC South', 'NFC'),
-        ('Tennessee', 'Titans', 'AFC East', 'AFC'),
-        ('Washington', 'Commanders', 'NFC East', 'NFC');
+Alter Table NFC
+Add constraint fk_NFCteam_id
+Foreign Key(teamID) References Teams(id);
 
-SELECT * FROM Teams;
+Insert into NFC(teamId, City, Mascot, Division) Select id, City, Mascot, Division from Teams
+Where Conference = 'NFC';
 
+Alter Table NFC
+Alter Wins
+Set Default 0;
 
+Update NFC Set Wins = 0;
+Update NFC Set Losses = 0;
+Update NFC Set ConferenceWins = 0;
+Update NFC Set ConferenceLosses = 0;
+
+Select * from NFC;
