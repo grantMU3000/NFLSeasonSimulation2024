7b0ea4ef7ae1f2d343ea79f7c0cd55eda3bd3b59
Grant <robinsonjg64@gmail.com>
2025-02-22 07:44:46 -0500

    Added in all of my AFC teams to a table

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -25,4 +25,25 @@ Update NFC Set Losses = 0;
 Update NFC Set ConferenceWins = 0;
 Update NFC Set ConferenceLosses = 0;
 
-Select * from NFC;
+Create Table AFC (
+	teamID int Primary Key,
+    City varchar(50),
+    Mascot varchar(50) Unique,
+    Wins int,
+    Losses int,
+    ConferenceWins int,
+    ConferenceLosses int,
+    Division varchar(25),
+    Foreign Key(TeamID) References Teams(id)
+);
+
+Alter Table AFC Alter Wins Set Default 0;
+Alter Table AFC Alter Losses Set Default 0;
+Alter Table AFC Alter ConferenceWins Set Default 0;
+Alter Table AFC Alter ConferenceLosses Set Default 0;
+
+Insert Into AFC(teamId, City, Mascot, Division) 
+Select id, City, Mascot, Division from Teams
+Where Conference = 'AFC';
+
+Select * from AFC;
